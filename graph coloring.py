0bfa7391433fe621e6e884fc6f5402da3ccb9011
Khushi Mundra <56793707+mundrak@users.noreply.github.com>
2021-10-10 17:49:04 +0530

    Graph coloring and unification problems
    Python programs for Graph coloring and unification problems in Artificial Intelligence

---
+++ graph coloring.py
@@ -0,0 +1,42 @@
+class Graph:
+ 
+    def _init_(self, edges, N):
+ 
+        self.adj = [[] for _ in range(N)]
+ 
+        for (src, dest) in edges:
+          self.adj[src].append(dest)
+          self.adj[dest].append(src)
+ 
+ 
+def colorGraph(graph):
+ 
+    result = {}
+ 
+    for u in range(N):
+ 
+        assigned = set([result.get(i) for i in graph.adj[u] if i in result])
+ 
+        color = 1
+        for c in assigned:
+            if color != c:
+                break
+            color = color + 1
+ 
+        result[u] = color
+ 
+    for v in range(N):
+        print("Color assigned to", v, "vertex is", colors[result[v]])
+ 
+ 
+if _name_ == '_main_':
+ 
+    colors = ["", "BLUE", "GREEN", "RED", "YELLOW", "ORANGE", "PINK", "BLACK", "BROWN", "WHITE", "PURPLE", "VOILET"]
+ 
+    edges = [(0, 1), (0, 4), (0, 5), (4, 5), (1, 4), (1, 3), (2, 3), (2, 4)]
+ 
+    N = 6
+ 
+    graph = Graph(edges, N)
+ 
+    colorGraph(graph)
